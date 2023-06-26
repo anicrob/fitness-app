3311c6c433d9c3b27f393eafe604676572a75fcc
Jack Bulger <127152910+jebulger@users.noreply.github.com>
2023-06-26 19:57:12 -0400

    Reverting logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -7,14 +7,9 @@ const logout = async () => {
   if (response.ok) {
     document.location.replace('/');
   } else {
-    // Dedicated error page is not created yet, but due to rare chance of logout error
-    // we should find a way to direct the user to an error page instead of displaying
-    // it right to the page
-    document.location.replace('/error');
-
-    // const { message } = await response.json();
-    // // eslint-disable-next-line no-undef
-    // showAlert({ message, type: 'danger' });
+    const { message } = await response.json();
+    // eslint-disable-next-line no-undef
+    showAlert({ message, type: 'danger' });
   }
 };
 
