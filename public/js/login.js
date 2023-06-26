aa50556ad34df6051a9482b02c90d1e117e15278
Jack Bulger <127152910+jebulger@users.noreply.github.com>
2023-06-26 19:56:40 -0400

    Reverting login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,54 +1,24 @@
 const loginFormHandler = async event => {
   event.preventDefault();
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
 
-  // Grabbing user inputs
-  const emailInput = document.querySelector('#email').value.trim();
-  const passwordInput = document.querySelector('#password').value.trim();
-
-  // The container doesn't exist yet, but this is for displaying a message to the user
-  // letting them know that the required fields were not entered to login
-  const errContainer = document.querySelector('#loginErr');
-
-  // Checks if fields are empty
-  if (!emailInput) {
-    showErr('You must enter the email associated with your account.', errContainer);
-    return;
-  }
-
-  if (!passwordInput) {
-    showErr('You must enter in the password to your account.', errContainer);
-    return;
-  }
-
-  // Will send the request once the required fields are entered
-  if (emailInput && passwordInput) {
+  if (email && password) {
     const response = await fetch('/api/login', {
       method: 'POST',
-      body: JSON.stringify({
-        email: emailInput,
-        password: passwordInput,
-      }),
+      body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
       document.location.replace('/dashboard');
     } else {
-      // const { message } = await response.json();
+      const { message } = await response.json();
       // eslint-disable-next-line no-undef
-      // showAlert({ target: 'login-alert', message, type: 'danger' });
-
-      // Generic error message, I might still need to find a better way to handle
-      // the an error in this case, as it could be server side or due to 
-      showErr('An error has occured during login. Please try again and make sure your credientials are correct.', errContainer);
+      showAlert({ target: 'login-alert', message, type: 'danger' });
     }
   }
 };
-
-// Updating container to list the field(s) that is mising
-const showErr = (message, errContainer) => {
-  errContainer.textContent += message + ' ';
-}
 
 document
   .querySelector('.login-form')
