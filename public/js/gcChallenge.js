dbc3aca63e3ed1c7d9f0a6a785982b8358fffac2
goose <anrobbins18@gmail.com>
2023-06-27 15:47:56 -0400

    updated comment in file

--- public/js/gcChallenge.js
+++ public/js/gcChallenge.js
@@ -2,5 +2,6 @@
 //event listeners for both buttons
 //get the challenge id from the challenge button
 //put request to challenge with completed: either true or false and current:false
-//PUT request to user to update the number of completed challenges
+//GET user data - pull numofCompleted exercises into a variable
+//PUT request to user to update the number of completed challenges +1 if completed = true
 //refresh page to remove the challenge from the page
