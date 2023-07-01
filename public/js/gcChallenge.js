c5d5723be3ce8df7a0b1dadda5cfe3be139a40bd
js131993 <john.sanders4444@gmail.com>
2023-07-01 11:41:54 -0400

    Adding give up and completed function

--- public/js/gcChallenge.js
+++ public/js/gcChallenge.js
@@ -4,3 +4,38 @@
 //put request to challenge with completed: either true or false and current:false
 //put request to user /numCompletedChallenges
 //refresh page to remove the challenge from the page
+
+const updateChallenge = async (id, completed) => {
+    const response = await fetch(`/api/challenges/${id}`, {
+      method: 'PUT',
+      body: {
+        completed,
+        current: false,
+      },
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      // document.location.replace('/');
+    } else {
+      alert('This exercise has already been added!');
+      return;
+    }
+  };
+  const completeChallenge = async id => {
+    await updateChallenge(id, true);
+  
+    const updateProfile = await fetch('/api/users/numCompletedChallenges', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (updateProfile.ok) {
+      //document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  const giveUpChallenge = async id => {
+    await updateChallenge(id, false);
+  };
