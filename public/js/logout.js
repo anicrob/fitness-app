6f1526683d489ce15a37c5bcb76759ff5bad0a27
SparkkyJD <127361245+SparkkyJD@users.noreply.github.com>
2023-07-01 21:17:43 -0400

    Update logout.js
    added parameters to log out user when inactive for 15 mins, and displays to handlebars when the user has 30 more seconds of session time. Also made it so the message will go away when a click or keypress event listener is triggered and restart the 15 min timer.

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,4 +1,3 @@
-//logout functionality
 const logout = async () => {
   const response = await fetch('/api/users/logout', {
     method: 'POST',
@@ -11,5 +10,51 @@ const logout = async () => {
     alert(response.statusText);
   }
 };
-//event listener
-document.querySelector('#logout').addEventListener('click', logout);
+
+let logoutTimer;
+let messageTimeout;
+
+const resetTimers = () => {
+  clearTimeout(logoutTimer);
+  clearTimeout(messageTimeout);
+  // starts a logout timer for 15 mins
+  logoutTimer = setTimeout(() => {
+    // logs the user out after 15 mins has elapsed
+    logout();
+  }, 900000);
+
+  // renders an alert to handlebars when there is 30 seconds left of session time.
+  messageTimeout = setTimeout(() => {
+    document.querySelector('#sessionTimeoutMessage').textContent = 'Your session will expire in 30 seconds.';
+  }, 870000);
+};
+// sets variable to clear message when the event listener is triggered
+const clearMessage = () => {
+  clearTimeout(messageTimeout);
+  document.querySelector('#sessionTimeoutMessage').textContent = '';
+};
+// logout params 
+document.querySelector('#logout').addEventListener('click', () => {
+  clearTimeout(logoutTimer);
+  clearTimeout(messageTimeout);
+  resetTimers();
+  clearMessage();
+});
+
+// event listeners to reset the timers when either listener is triggered
+document.addEventListener('click', () => {
+  resetTimers();
+  clearMessage();
+  setTimeout(() => {
+    document.querySelector('#sessionTimeoutMessage').textContent = 'Your session will expire in 30 seconds.';
+  }, 870000);
+});
+document.addEventListener('keypress', () => {
+  resetTimers();
+  clearMessage();
+  setTimeout(() => {
+    document.querySelector('#sessionTimeoutMessage').textContent = 'Your session will expire in 30 seconds.';
+  }, 870000);
+});
+
+resetTimers();
